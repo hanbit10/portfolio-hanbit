c6be61830cba882c8542e152451ec8703cec481d
hanbit10 <chanbit10@gmail.com>
2024-07-08 15:16:21 +0200

    Local download font

--- src/app/page/impressum/impressum.component.ts
+++ src/app/page/impressum/impressum.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './impressum.component.html',
-  styleUrl: './impressum.component.scss'
+  styleUrl: './impressum.component.scss',
 })
-export class ImpressumComponent {
-
-}
+export class ImpressumComponent {}
--- src/app/page/main-page/main-page.component.ts
+++ src/app/page/main-page/main-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-
-}
+export class MainPageComponent {}
