ab10a8a8f2a3f0280623042298327813bd773496
hanbit10 <chanbit10@gmail.com>
2024-07-10 20:41:51 +0200

    update footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './page/main-page/main-page.component';
+import { ImpressumComponent } from './page/impressum/impressum.component';
 
-export const routes: Routes = [{ path: '', component: MainPageComponent }];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'impressum', component: ImpressumComponent },
+];
--- src/app/page/impressum/impressum.component.ts
+++ src/app/page/impressum/impressum.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss',
 })
-export class ImpressumComponent {}
+export class ImpressumComponent implements OnInit {
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  constructor(private router: Router) {}
+}
