1b0c5e4569f4b933e1a06e1b648c801d693cca8b
hanbit10 <chanbit10@gmail.com>
2024-07-10 23:19:55 +0200

    update head component

--- src/app/page/main-page/main-page.component.ts
+++ src/app/page/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
