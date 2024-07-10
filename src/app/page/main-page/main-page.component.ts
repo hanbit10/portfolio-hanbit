ea95ce6e664126ffebf5121b66ac0625330cf159
hanbit10 <chanbit10@gmail.com>
2024-07-10 23:27:23 +0200

    update header component

--- src/app/page/main-page/main-page.component.ts
+++ src/app/page/main-page/main-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from '../../components/profile/profile.component';
+import { AboutComponent } from '../../components/about/about.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [
+    FooterComponent,
+    HeaderComponent,
+    ProfileComponent,
+    AboutComponent,
+    CommonModule,
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {}
+export class MainPageComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+}
