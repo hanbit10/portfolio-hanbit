483144c59e70f8f80014b0fb5bf09c77c4c48190
hanbit10 <chanbit10@gmail.com>
2024-07-16 22:52:11 +0200

    update

--- src/app/components/portfolios/portfolios.component.ts
+++ src/app/components/portfolios/portfolios.component.ts
@@ -13,9 +13,20 @@ export class PortfoliosComponent implements OnInit {
   ngOnInit(): void {
     for (let i = 1; i <= 4; i++) {
       if (i % 2 == 0) {
-        let porfolios = document.getElementById(`${i}`);
-        console.log(porfolios);
-        porfolios?.classList.add('right-position');
+        let portfolios = document.getElementById(`${i}`);
+        // console.log(portfolios);
+        portfolios?.classList.add('right-position');
+        let info = portfolios?.querySelector(`.portfolio-info`);
+        // console.log('portfolios', portfolios);
+        // console.log('info', info);
+        let title = info?.querySelector('#title');
+        let skills = info?.querySelector('#skills');
+        let description = info?.querySelector('#description');
+        let btnContainer = info?.querySelector('.btn-container');
+        title?.classList.add('text-end');
+        skills?.classList.add('text-end');
+        description?.classList.add('text-end');
+        btnContainer?.classList.add('f-end');
       }
     }
   }
