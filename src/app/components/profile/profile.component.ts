31369775d0fd13856cb55120c93b48db3c5c81fd
hanbit10 <chanbit10@gmail.com>
2024-07-16 16:42:36 +0200

    update portfolios

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
-
-}
+export class ProfileComponent {}
