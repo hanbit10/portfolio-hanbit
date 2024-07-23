65f079f6058911835feaff30e7365b5a8331ef4a
hanbit10 <chanbit10@gmail.com>
2024-07-23 17:06:59 +0200

    update imprint and privacy

--- src/app/page/privacy-policy/privacy-policy.component.ts
+++ src/app/page/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
 })
