69bca5db5e970be46342c842b4e2bee0b05287fb
1bitcode <38600431+hanbit10@users.noreply.github.com>
2026-03-28 14:26:37 +0000

    update image config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { IMAGE_CONFIG } from '@angular/common';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(),
+        {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true, // This kills the NG0913 warning
+      }
+    },
+  ],
 };
