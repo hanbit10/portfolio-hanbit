96b836f16ef97a70125f7ff963562461a72c6f10
1bitcode <38600431+hanbit10@users.noreply.github.com>
2026-06-24 19:08:56 +0200

    php test

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
